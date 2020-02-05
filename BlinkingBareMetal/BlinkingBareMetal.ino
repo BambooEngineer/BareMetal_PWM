fa3bca1ae3b383d2dfea40bbbf8fdd03772956ed
BambooEngineer <37255295+BambooEngineer@users.noreply.github.com>
2020-02-05 13:01:14 -0500

    Update BlinkingBareMetal.ino

--- BlinkingBareMetal/BlinkingBareMetal.ino
+++ BlinkingBareMetal/BlinkingBareMetal.ino
@@ -8,12 +8,12 @@ uint16_t x;
 
 void setup() {
   
-  DDRB |= _BV(DDB5);
+  DDRB |= _BV(DDB5); // pin 13 as output
   
-  DDRC = 0b00000000; 
-  ADMUX = 0b01100000; 
+  DDRC = 0b00000000; // analog 0 as input 
+  ADMUX = 0b01100000; // Multiplexer configuration
   
-  ADCSRA = 0b10000111;  
+  ADCSRA = 0b10000111;  // Prescaler configuration
   
 
 }
@@ -21,26 +21,26 @@ void setup() {
 void loop() {
 
   
-  ADCSRA = ADCSRA | (1 << ADSC);
+  ADCSRA = ADCSRA | (1 << ADSC); // Start ADC Conversion
 
   
-  while(ADCSRA & (1<<ADSC));
+  while(ADCSRA & (1<<ADSC)); // Wait till bit is cleared
 
-  x = ADCH;
+  x = ADCH; // x as potentiometer value 
 
-  PWM(x);
+  PWM(x); // PWM function
 
 }
 
 void PWM(uint16_t x){
   
-  PORTB |= _BV(PORTB5);
+  PORTB |= _BV(PORTB5); // turn on pin 13
 
   
-  _delay_loop_2(x);
+  _delay_loop_2(x); // delay according to potentiometer value 
  
 
-  PORTB &= ~_BV(PORTB5);
+  PORTB &= ~_BV(PORTB5); // turn off pin 13
   
-  _delay_loop_2(x);
+  _delay_loop_2(x); // another delay
 }
